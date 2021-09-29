02b813eff706c25c9db6bff9b65b4e3fdd1db363
Jaswanth Surabattula <thisisjash619@gmail.com>
2021-09-29 22:53:58 +0530

    Time: 360 ms (9.18%), Space: 100.7 MB (6.26%) - LeetHub

---
+++ contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        map <int, int> rec;
+        map <int, int> freq;
+        int n = nums.size ();
+        
+        for (int i = 0; i < n; i++) {
+            if (freq [nums [i]] == 0) {
+                rec [nums [i]] = i;
+                freq [nums [i]]++;
+            }
+            else {
+                if (nums [i] == nums [rec [nums [i]]]) {
+                    if (i - rec [nums [i]] <= k) {
+                        cout << i;
+                        return true;
+                    }    
+                    else {
+                        rec [nums [i]] = i;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
