32e873fc82faa9a1c69d6c0974347590fb728941
Jaswanth Surabattula <thisisjash619@gmail.com>
2021-09-30 23:04:46 +0530

    Time: 4 ms (87.62%), Space: 10.7 MB (28.76%) - LeetHub

---
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        map <int, int> freq;
+        int m = nums1.size (), n = nums2.size ();
+        vector <int> vec;
+        
+        for (int i = 0; i < m; i++)
+                freq [nums1 [i]]++;
+        
+        for (int i = 0; i < n; i++) {
+            if (freq [nums2 [i]] > 0)
+                vec.push_back (nums2 [i]);
+            freq [nums2 [i]]--;
+        }
+        
+        return vec;
+    }
+};
