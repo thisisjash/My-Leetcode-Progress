3e9fc8827c7425632a458f7b94da1ff91257a1de
Jaswanth Surabattula <thisisjash619@gmail.com>
2021-10-03 08:01:23 +0530

    Time: 56 ms (82.88%), Space: 48.4 MB (49.19%) - LeetHub

--- jump-game/jump-game.cpp
+++ jump-game/jump-game.cpp
@@ -5,7 +5,6 @@ public:
         int n = nums.size ();
         for (int i = 0; i < n && i <= maxReach; i++) {
             maxReach = maxReach > (nums [i] + i) ? maxReach : nums [i] + i;
-            // cout << i << " " << maxReach << endl;
             if (maxReach >= n - 1)
                 return true;
         }
